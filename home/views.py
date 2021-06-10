8d8d72aaff9e49f524307477cfdff90e01c92d96
francesco-saponaro <francescosaponaro5@gmail.com>
2021-06-10 19:51:48 +0000

    Fix cryptocurrencies table layout on all screen sizes

--- home/views.py
+++ home/views.py
@@ -7,4 +7,6 @@ def index(request):
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C%20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
 
-    return render(request, 'home/index.html')
+    context = {'data': data}
+
+    return render(request, 'home/index.html', context)

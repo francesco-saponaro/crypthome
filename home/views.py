0b4f216b8bc6f06fb1eff05392cc71a304ddd356
francesco-saponaro <francescosaponaro5@gmail.com>
2021-06-10 13:19:43 +0000

    Create and migrate cryptocurrency model

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
 def index(request):
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C%20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data = requests.get(url).json()
+
     return render(request, 'home/index.html')

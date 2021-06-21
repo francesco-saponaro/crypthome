062aa06ad3274f94b367276d17d9d683450d49fe
francesco-saponaro <francescosaponaro5@gmail.com>
2021-06-21 16:36:39 +0000

    Create buy page and relative websocket path

--- home/static/home/js/websocket.js
+++ home/static/home/js/websocket.js
@@ -3,148 +3,48 @@ let socket = new WebSocket(`wss://${window.location.host}/ws/home/`);
 
 /* Add data coming from the consumer to a variable */
 socket.onmessage = function(event) {
-    let tokens = JSON.parse(event.data);
-    console.log(tokens);
+    let ws_tokens = JSON.parse(event.data);
+    console.log(ws_tokens);
 
     /* Create a variable to populate the large table, that loops through each token and creates a row with token data for each */
-    let newDataLg = tokens.map(token => 
+    let newDataLg = ws_tokens.map(ws_token => 
         `<tr>
             <td class="align-middle"><i class="far fa-star favourites"></i></td>
-            <td class="align-middle">${token.rank}</td>
+            <td class="align-middle">${ws_token.rank}</td>
             <td class="align-middle text-start">
-                <a href="${token.id}" class="text-light link">
-                    <img src=${token.image} height="30"> 
-                    ${token.name}
-                    <span class="text-uppercase fw-bold symbol">${token.symbol}</span>
+                <a href="token_page/${ws_token.id}/" class="text-light link">
+                    <img src=${ws_token.image} height="30"> 
+                    ${ws_token.name}
+                    <span class="text-uppercase fw-bold symbol">${ws_token.symbol}</span>
                 </a>
             </td>
             <td class="align-middle">
-                <!-- Buy button trigger modal -->
-                <button type="button" class="btn rounded-0 text-uppercase" data-bs-toggle="modal" data-bs-target="#staticBackdrop${token.name}">
-                    Buy
-                </button>
-
-                <!-- Modal -->
-                <div class="modal fade" id="staticBackdrop${token.name}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content crypto-modal">
-                            <div class="modal-header text-light">
-                                <h5 class="modal-title text-uppercase" id="staticBackdropLabel">Buy ${token.name}</h5>
-                                <button type="button" class="btn-close bg-secondary" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-
-                            <div class="modal-body">
-                                <!-- Token data -->
-                                <div class="row pb-3 modal-row">
-                                    <div class="col-6">
-                                        <p class="modal-data-h">Price</p>
-                                        <p class="modal-data mb-0">£${token.price}</p>
-                                    </div>
-                                    <div class="col-6">
-                                        <p class="modal-data-h">24h %</p>
-                                        <p class="modal-data mb-0">${token.price_change.toFixed(2)}%</p>
-                                    </div>
-                                </div>
-
-                                <!-- Monetary inputs -->
-                                <div class="row pt-3">
-                                    <div class="col-12 pb-3 text-start">
-                                        <label for="token-amount" class="pb-1 modal-data-h">Amount ${token.name}</label>
-                                        <input class="form-control border-0 rounded-0 bg-secondary text-center" type="number" id="token-amount" name="token-amount" placeholder="Amount ${ token.name}">
-                                    </div>
-                                    <div class="col-12 text-start">
-                                        <label for="gbp-amount" class="pb-1 modal-data-h">Total (GBP)</label>
-                                        <input class="form-control border-0 rounded-0 bg-secondary text-center" type="number" id="gbp-amount" name="gbp-amount" placeholder="Total (GBP)">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="modal-footer">
-                                <!-- Cancel and buy buttons -->
-                                <button type="button" class="btn cancel-sell-btn" data-bs-dismiss="modal">
-                                    Cancel
-                                </button>
-                                <button type="button" class="btn">
-                                    Buy
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <!-- Buy button -->
+                <a href="buy_token/${ws_token.id}/" class="btn rounded-0 text-uppercase">Buy</a>
             </td>
-            <td class="align-middle text-end ${token.direction === "higher" ? "higher" : token.direction === "lower" ? "lower" : "same"}">${token.price}</td>
-            <td class="align-middle text-end">${token.price_change.toFixed(2)}</td>
-            <td class="align-middle text-end">${token.market_cap}</td>
+            <td class="align-middle text-end ${ws_token.direction === "higher" ? "higher" : ws_token.direction === "lower" ? "lower" : "same"}">${ws_token.price}</td>
+            <td class="align-middle text-end">${ws_token.price_change.toFixed(2)}</td>
+            <td class="align-middle text-end">${ws_token.market_cap}</td>
         </tr>`
     );
 
     /* Create a variable to populate the small table, that loops through each token and creates a row with token data for each */
-    let newDataSm = tokens.map(token => 
+    let newDataSm = ws_tokens.map(ws_token => 
         `<tr>
-            <td class="align-middle"><img src=${token.image} height="30"></td>
+            <td class="align-middle"><img src=${ws_token.image} height="30"></td>
             <td class="text-start">
-                <a href="${token.id}" class="text-light link">
-                    ${token.name}
-                    <br><span class="bg-dark btn-sm">${token.rank}</span>
-                    <span class="text-uppercase fw-bold x-small">${token.symbol}  ${token.price_change.toFixed(2)}%</span>
+                <a href="token_page/${ws_token.id}/" class="text-light link">
+                    ${ws_token.name}
+                    <br><span class="bg-dark btn-sm">${ws_token.rank}</span>
+                    <span class="text-uppercase fw-bold x-small">${ws_token.symbol}  ${ws_token.price_change.toFixed(2)}%</span>
                 </a>
             </td>
             <td class="align-middle">
-                <!-- Buy button trigger modal -->
-                <button type="button" class="btn rounded-0 text-uppercase" data-bs-toggle="modal" data-bs-target="#staticBackdropSmall${token.name}">
-                    Buy
-                </button>
-
-                <!-- Modal -->
-                <div class="modal fade" id="staticBackdropSmall${token.name}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content crypto-modal">
-                            <div class="modal-header text-light">
-                                <h5 class="modal-title text-uppercase" id="staticBackdropLabel">Buy ${token.name}</h5>
-                                <button type="button" class="btn-close bg-secondary" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-
-                            <div class="modal-body">
-                                <!-- Token data -->
-                                <div class="row pb-3 modal-row">
-                                    <div class="col-6">
-                                        <p class="modal-data-h">Price</p>
-                                        <p class="modal-data mb-0">£${token.price}</p>
-                                    </div>
-                                    <div class="col-6">
-                                        <p class="modal-data-h">24h %</p>
-                                        <p class="modal-data mb-0">${token.price_change.toFixed(2)}%</p>
-                                    </div>
-                                </div>
-
-                                <!-- Monetary inputs -->
-                                <div class="row pt-3">
-                                    <div class="col-12 pb-3 text-start">
-                                        <label for="token-amount" class="pb-1 modal-data-h">Amount ${token.name}</label>
-                                        <input class="form-control border-0 rounded-0 bg-secondary text-center" type="number" id="token-amount" name="token-amount" placeholder="Amount ${ token.name}">
-                                    </div>
-                                    <div class="col-12 text-start">
-                                        <label for="gbp-amount" class="pb-1 modal-data-h">Total (GBP)</label>
-                                        <input class="form-control border-0 rounded-0 bg-secondary text-center" type="number" id="gbp-amount" name="gbp-amount" placeholder="Total (GBP)">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="modal-footer">
-                                <!-- Cancel and buy buttons -->
-                                <button type="button" class="btn cancel-sell-btn" data-bs-dismiss="modal">
-                                    Cancel
-                                </button>
-                                <button type="button" class="btn">
-                                    Buy
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <!-- Buy button -->
+                <a href="buy_token/${ws_token.id}/" class="btn rounded-0 text-uppercase">Buy</a>
             </td>
-            <td class="align-middle text-end price ${token.direction === "higher" ? "higher" : token.direction === "lower" ? "lower" : "same"}">£${token.price}
-                <br><span class="x-small text-light">MCap £${token.market_cap}</span>
+            <td class="align-middle text-end price ${ws_token.direction === "higher" ? "higher" : ws_token.direction === "lower" ? "lower" : "same"}">£${ws_token.price}
+                <br><span class="x-small text-light">MCap £${ws_token.market_cap}</span>
             </td>
         </tr>`
     );
--- home/views.py
+++ home/views.py
@@ -12,7 +12,7 @@ def index(request):
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
 
-    context = {'data': data}
+    context = {'api_data': data}
 
     return render(request, 'home/index.html', context)
 
@@ -25,7 +25,7 @@ def watchlist(request, **kwargs):
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
 
-    context = {'data': data}
+    context = {'api_data': data}
 
     return render(request, 'home/watchlist.html', context)
 
@@ -42,8 +42,27 @@ def token_page(request, token_id):
     token = get_object_or_404(Position, pk=token_id)
 
     context = {
-        'data': data,
-        'token': token,
+        'api_data': data,
+        'db_token': token,
         }
 
     return render(request, 'home/token_page.html', context)
+
+
+# Buy token view
+def buy_token(request, token_id):
+    # Coingecko API
+    url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=\
+           bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C\
+           %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    data = requests.get(url).json()
+
+    # Get object from position model
+    token = get_object_or_404(Position, pk=token_id)
+
+    context = {
+        'api_data': data,
+        'db_token': token,
+        }
+
+    return render(request, 'home/buy_token.html', context)

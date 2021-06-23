d3df688960ddfc9a4ed7bc50b80254931a3af2de
francesco-saponaro <francescosaponaro5@gmail.com>
2021-06-23 16:01:02 +0000

    Complete token page

---
+++ home/static/home/js/showMore.js
@@ -0,0 +1,31 @@
+function toggleText() {
+    // Get all the elements from the page
+    let dots = document.getElementById("dots");
+    let moreText = document.getElementById("more-text");
+    let buttonText = document.getElementById("text-button");
+  
+    // If the display property of the dots to be displayed is already set to 'none' (that is hidden) then this 
+    // section of code triggers
+    if (dots.style.display === "none") {
+        // Show the dots after the text
+        dots.style.display = "inline";
+
+        // Hide the text between the span elements
+        moreText.style.display = "none";
+
+        // Change the text on button to 'Show More' and the icon to caret down
+        buttonText.innerHTML = "Show More <i class='fas fa-caret-down'></i>";
+    }
+
+    // If the hidden portion is revealed, we will change it back to be hidden
+    else {
+        // Hide the dots after the text
+        dots.style.display = "none";
+        
+        // Show the text between the span elements
+        moreText.style.display = "inline";
+
+        // Change the text on button to 'Show Less' and the icon to caret up
+        buttonText.innerHTML = "Show Less <i class='fas fa-caret-up'></i>";
+    }
+}
--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ import requests
 
 # Home page view
 def index(request):
-    # Coingecko API
+    # Get coins data from Coingecko API
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=\
            bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C\
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
@@ -19,7 +19,7 @@ def index(request):
 
 # Watchlist page view
 def watchlist(request, **kwargs):
-    # Coingecko API
+    # Get coins data from Coingecko API
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=\
            bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C\
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
@@ -32,18 +32,102 @@ def watchlist(request, **kwargs):
 
 # Product page view
 def token_page(request, token_id):
-    # Coingecko API
+    # Get coins data from Coingecko API
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=\
            bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C\
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
 
-    # Get object from position model
+    # Get detailed coin data for the targeted token from Coingecko API
+    coins_url = f'https://api.coingecko.com/api/v3/coins/{token_id}'
+    coins_data = requests.get(coins_url).json()
+
+    # Get description value from detailed coin data and split it in two parts
+    # for the JS text-hiding function.
+    # Replace anchor links with empty string.
+    # Unfortunately I had to copy exaxt matched string to replace
+    # as the following regex were not working somehow, I have taken
+    # this regex from stack overflow but were not working. Even the
+    # tutors were not able to find the issue as the regex are valid
+    # and were tested with a regex validator.
+    # ("<(?:a\b[^>]*>|/a>)", "")
+    # First part.
+    coins_desc = coins_data["description"]["en"][:950]\
+        .replace('<a href="https://www.coingecko.com/en?hashing_algorithm=SHA-256">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/litecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/peercoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/primecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/eos">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/tron">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/ethereum-still-king-dapps-cryptokitties-need-1-billion-on-eos">', "")\
+        .replace('<a href="https://www.coingecko.com/en?category_id=29&view=market">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum_classic">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/qtum">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/neo">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/icon">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/cardano">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/complete-beginners-guide-to-metamask?locale=en">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/trezor-model-t-wallet-review">', "")\
+        .replace('<a href="https://www.coingecko.com/en?category_id=29">', "")\
+        .replace('<a href="https://www.coingecko.com/en?hashing_algorithm=Proof+of+Stake&view=market">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/bitcoin">', "")\
+        .replace('<a href="https://daedaluswallet.io/">', "")\
+        .replace('<a href="https://iohk.io/">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum-classic">', "")\
+        .replace('<a href="https://cardanofoundation.org/en/">', "")\
+        .replace('<a href="https://iohk.io/team/charles-hoskinson/">', "")\
+        .replace('<a href="https://iohk.io/team/jeremy-wood/">', "")\
+        .replace('<a href="https://emurgo.io/">', "")\
+        .replace('<a href="https://iohk.io/team/charles-hoskinson/">', "")\
+        .replace('<a href="https://www.crunchbase.com/organization/ripple-labs">', "")\
+        .replace('<a href="/coins/litecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/how-to-use-an-ethereum-wallet">', "")\
+        .replace("</a>", "")
+
+    # Second part.
+    coins_desc_second = coins_data["description"]["en"][950:]\
+        .replace('<a href="https://www.coingecko.com/en?hashing_algorithm=SHA-256">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/litecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/peercoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/primecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/eos">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/tron">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/ethereum-still-king-dapps-cryptokitties-need-1-billion-on-eos">', "")\
+        .replace('<a href="https://www.coingecko.com/en?category_id=29&view=market">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum_classic">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/qtum">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/neo">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/icon">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/cardano">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/complete-beginners-guide-to-metamask?locale=en">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/trezor-model-t-wallet-review">', "")\
+        .replace('<a href="https://www.coingecko.com/en?category_id=29">', "")\
+        .replace('<a href="https://www.coingecko.com/en?hashing_algorithm=Proof+of+Stake&view=market">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/bitcoin">', "")\
+        .replace('<a href="https://daedaluswallet.io/">', "")\
+        .replace('<a href="https://iohk.io/">', "")\
+        .replace('<a href="https://www.coingecko.com/en/coins/ethereum-classic">', "")\
+        .replace('<a href="https://cardanofoundation.org/en/">', "")\
+        .replace('<a href="https://iohk.io/team/charles-hoskinson/">', "")\
+        .replace('<a href="https://iohk.io/team/jeremy-wood/">', "")\
+        .replace('<a href="https://emurgo.io/">', "")\
+        .replace('<a href="https://iohk.io/team/charles-hoskinson/">', "")\
+        .replace('<a href="https://www.crunchbase.com/organization/ripple-labs">', "")\
+        .replace('<a href="/coins/litecoin">', "")\
+        .replace('<a href="https://www.coingecko.com/buzz/how-to-use-an-ethereum-wallet">', "")\
+        .replace("</a>", "")
+
+    # Get targeted token object from position model
     token = get_object_or_404(Position, pk=token_id)
 
     context = {
         'api_data': data,
         'db_token': token,
+        'coins_api_data': coins_data,
+        'coins_api_desc': coins_desc,
+        'coins_api_desc_second': coins_desc_second,
         }
 
     return render(request, 'home/token_page.html', context)
@@ -51,13 +135,13 @@ def token_page(request, token_id):
 
 # Buy token view
 def buy_token(request, token_id):
-    # Coingecko API
+    # Get coins data from Coingecko API
     url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=\
            bitcoin%2C%20ethereum%2C%20cardano%2C%20dogecoin%2C%20polkadot%2C\
            %20ripple&order=market_cap_desc&per_page=100&page=1&sparkline=false'
     data = requests.get(url).json()
 
-    # Get object from position model
+    # Get targeted token object from position model
     token = get_object_or_404(Position, pk=token_id)
 
     context = {

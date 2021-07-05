103eab8bff55437363ddc2966aafe261e0533be5
francesco-saponaro <francescosaponaro5@gmail.com>
2021-07-05 11:39:39 +0000

    Add merch list display and ordering parameters in admin models

--- merch/admin.py
+++ merch/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Category, Merch
 
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Merch)
+class MerchAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Merch, MerchAdmin)
--- merch/models.py
+++ merch/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +17,10 @@ class Category(models.Model):
 
 
 class Merch(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Merch'
+
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)

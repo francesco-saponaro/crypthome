391059a1a5e5aef7b9d339f5cefc7594fd9ecb87
francesco-saponaro <francescosaponaro5@gmail.com>
2021-08-09 15:28:07 +0000

    Replaced secret key setting to get it from environment

--- crypthome/settings.py
+++ crypthome/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&=l6nfy7qe#dwmslhm8hf#\
-              ^xh5=ezv2gq*c-yp%=q32b*9*0o'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['crypthome.herokuapp.com', 'localhost']
 
